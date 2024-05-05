ae355eb8e1bd4c92ac5b388d97e70a235a599b44
DCPetty <github@patton-petty.net>
2024-05-04 20:43:40 -0400

    Temporarily wiping out src.

--- src/alternative/d.py
+++
@@ -1,9 +0,0 @@
-# d.py
-
-import logging
-FORMAT = '%(levelname)s:%(name)20s:%(message)s'
-logging.basicConfig(format=FORMAT, level='INFO')
-logger = logging.getLogger(__name__)
-logger.info(f" \u2191 d.py (__package__={repr(__package__)})")
-
-d = 'd'
--- src/alternative/package/b.py
+++
@@ -1,9 +0,0 @@
-# b.py
-
-import logging
-FORMAT = '%(levelname)s:%(name)20s:%(message)s'
-logging.basicConfig(format=FORMAT, level='INFO')
-logger = logging.getLogger(__name__)
-logger.info(f" \u2191 b.py (__package__={repr(__package__)})")
-
-b = 'b'
