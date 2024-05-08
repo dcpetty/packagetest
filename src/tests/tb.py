ea202058bccd3357eaefa086f966e46c076f3517
DCPetty <github@patton-petty.net>
2024-05-08 19:25:07 -0400

    Fixed comment.

--- src/tests/tb.py
+++ src/tests/tb.py
@@ -4,7 +4,7 @@ import logging as _logging
 FORMAT = '%(levelname)s:%(name)10s:%(message)s'
 _logging.basicConfig(format=FORMAT, level='INFO')
 _logger = _logging.getLogger(__name__)
-_logger.info(f" \u2191 ta.py (__package__={repr(__package__)})")
+_logger.info(f" \u2191 tb.py (__package__={repr(__package__)})")
 
 import unittest
 from . import b
